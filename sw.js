5b43c51dd12367f7f17e8fc1effc8ceaf63dcfd3
JesseHayes <jessehayes@live.com>
2026-02-16 21:13:59 -0400

    minor fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //Everytime new images are added include them in cache and bump name
 
-const CACHE_NAME = "nscraft-cache-v5";
+const CACHE_NAME = "nscraft-cache-v6";
 const urlsToCache = [
     "./",
     "./index.html",
